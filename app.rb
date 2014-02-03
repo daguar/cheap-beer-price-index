d512d11241de690ddc9f619a48af820bf7732c2f
Dave Guarino <dave@codeforamerica.org>
2014-02-03 10:05:40 -0800

    Use configure block instead of global

--- app.rb
+++ app.rb
@@ -1,18 +1,20 @@
 require 'sinatra'
 require 'yelpster'
 
-Yelp.configure(:consumer_key => ENV['CONSUMER_KEY'],\
+configure do
+  Yelp.configure(:consumer_key => ENV['CONSUMER_KEY'],\
   :consumer_secret => ENV['CONSUMER_SECRET'],\
   :token => ENV['TOKEN'],\
   :token_secret => ENV['TOKEN_SECRET'])
 
-@@client = Yelp::Client.new
+  set :client, Yelp::Client.new
+end
 
 get '/bars.json' do
   #return [] unless [:sw_latitude, :sw_longitude, :ne_latitude, :ne_longitude].all? { |key| params.key?(key) }
   params[:category_filter] = "bars"
   request = Yelp::V2::Search::Request::BoundingBox.new(params)
-  response = @@client.search(request)
+  response = settings.client.search(request)
   response.to_json
 end
 
