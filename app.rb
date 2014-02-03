7552f2352590356d5bd0ebf3f12978afa7806f14
Dave Guarino <dave@codeforamerica.org>
2014-02-03 14:55:02 -0800

    Initial work

--- app.rb
+++ app.rb
@@ -1,20 +1,19 @@
 require 'sinatra'
 require 'yelpster'
+require 'foursquare2'
 
 configure do
-  Yelp.configure(:consumer_key => ENV['CONSUMER_KEY'],\
-  :consumer_secret => ENV['CONSUMER_SECRET'],\
-  :token => ENV['TOKEN'],\
-  :token_secret => ENV['TOKEN_SECRET'])
-
-  set :client, Yelp::Client.new
+  set :client, Foursquare2::Client.new(:client_id => ENV['FOURSQUARE_CLIENT_ID'], :client_secret => ENV['FOURSQUARE_CLIENT_SECRET'])
 end
 
 get '/bars.json' do
   #return [] unless [:sw_latitude, :sw_longitude, :ne_latitude, :ne_longitude].all? { |key| params.key?(key) }
-  params[:category_filter] = "bars"
-  request = Yelp::V2::Search::Request::BoundingBox.new(params)
-  response = settings.client.search(request)
+  #params[:category_filter] = "bars"
+  puts client
+  puts params
+  map_sw = [params[:sw_latitude], params[:sw_longitude]]
+  map_ne = [params[:ne_latitude], params[:ne_longitude]]
+  response = client.search_venues(sw: map_sw, ne: map_ne)
   response.to_json
 end
 
